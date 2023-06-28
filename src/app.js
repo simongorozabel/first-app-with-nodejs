dc53b849be042a187194be68e4f90d19e22f9830
Simon Gorozabel <sgorozabel@gmail.com>
2023-06-27 21:11:50 -0500

    first commit

--- src/app.js
+++ src/app.js
@@ -1,19 +1,46 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-require('dotenv').config();
-const router = require('./routes');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+require("dotenv").config();
+const router = require("./routes");
+
+// linea 6
+const handlebars = require("express-handlebars");
 
 // Esta es nuestra aplicación
 const app = express();
 
-// Middlewares 
+// Middlewares
 app.use(express.json());
-app.use(helmet({
+app.use(
+  helmet({
     crossOriginResourcePolicy: false,
-}));
+  })
+);
+
+app.use(express.static(__dirname + "/public"));
+
+// linea 17
+app.set("view engine", "handlebars");
+app.set("views", __dirname + "/views");
+app.engine(
+  "handlebars",
+  handlebars.engine({
+    layoutsDir: __dirname + "/views/layouts",
+    defaultLayout: "index",
+  })
+);
 app.use(cors());
 
-app.use('/', router);
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      "img-src": ["'self'", "https: data:"],
+    },
+  })
+);
+
+app.use("/", router);
 
 module.exports = app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,31 @@
-const express = require('express');
+const express = require("express");
+const { type } = require("express/lib/response");
 const router = express.Router();
+const projects = require("../projects.json");
 
 // colocar las rutas aquí
-router.get('/', (req, res) => {
-    res.send("Welcome to express");
+router.get("/", (req, res) => {
+  res.render("home");
+});
+router.get("/about-us", (req, res) => {
+  res.render("about-us");
 });
 
+router.get("/projects", (req, res) => {
+  res.render("projects", { projects });
+});
+router.get("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  const projectById = projects.find((project) => project.id == id);
+  res.render("project-detail", { id, projectById });
+});
+
+router.get("/team", (req, res) => {
+  res.render("team");
+});
+router.get("/team/:name", (req, res) => {
+  const { name } = req.params;
+  res.render("team-member", { name });
+});
 
 module.exports = router;
